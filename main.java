86d7a76f3ad352feef29259ca1c6ca1367b26390
deformed-cactus <63313750+deformed-cactus@users.noreply.github.com>
2022-06-06 22:59:41 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.*;
+
+
+public class main{
+
+    public static void main(String args[]){
+
+      //make a scanner that reads in the desired txt file name, for conversion to pgn
+      Scanner scanner  = new Scanner(System.in);
+      System.out.println("Enter the file name, please.");
+      String fileName  = scanner.nextLine();
+      scanner.close();
+
+      //takes in the txt file containing moves, and adds in the periods so that a pgn is returned.
+      try{
+        File moves     = new File(fileName);
+        Scanner reader = new Scanner(moves);
+        while(reader.hasNextLine()){
+          String data  = reader.nextLine();
+          data = data.replaceFirst("\\t", ". ");
+          System.out.println(data);
+        }
+        reader.close();
+      } catch(FileNotFoundException error) {
+      System.out.println("An error occurred.");
+      error.printStackTrace();
+      }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,26 @@
+README
+
+This is a program that allows you to input algebraic notation from 2700chess.com
+and return a pgn.
+
+You need Java to run this program.
+
+Instructions to convert algebraic notation to pgn:
+First you need to copy the moves of the game (and only the
+moves) and save them as a .txt file. The file needs to be saved in the same
+folder as this program.
+
+Then, open your command line and navigate into the program's repository, which
+is called "pgn".
+
+To start the program, write java main.java in the command prompt and hit
+return/enter.
+
+The program will ask you for the name of the file where you saved the algebraic
+notation. Type in the name of that file and hit return/enter.
+
+The pgn notation is then returned. You can copy it from the command line into
+lichess analysis board for example, or into a word processor and save as a pgn.
+
+You can test the program on the "test.txt" file in the repository,
+which already has some algebraic notation that requires conversion to pgn.
